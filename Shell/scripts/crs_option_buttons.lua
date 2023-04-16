aff8e65ea85c2f6cfb53ecc575e57b43af09ff12
Ben R <milehighben@hotmail.com>
2023-04-15 22:49:40 -0700

    added globals, fixed dropdown display

--- Shell/scripts/crs_option_buttons.lua
+++ Shell/scripts/crs_option_buttons.lua
@@ -1,3 +1,25 @@
+
+
+
+--- define global to store the team faction choices
+
+gCrossEra = {
+    attackerName = "",
+    defenderName = "",
+    factions = {
+        { name = "rep", string = "common.sides.rep.name" },
+        { name = "cis", string = "common.sides.cis.name" },
+        { name = "all", string = "common.sides.all.name" },
+        { name = "imp", string = "common.sides.imp.name" }
+    }
+}
+
+gCrossEra.attackerName = gCrossEra.factions[1].name
+gCrossEra.defenderName = gCrossEra.factions[2].name
+
+
+
+
 --these functions reference the page tags
 -- ifs_io_changeFunc
 -- ifs_io_GetRealValueFor
@@ -15,6 +37,7 @@ add_crs_buttons = function()
     --TODO: pass faction name to mission script
     -- use remaster DB ?
 
+    --choose what setting to set based on selection
     --called when you change a radio button
     local original_ifs_io_changeFunc = ifs_io_changeFunc
     ifs_io_changeFunc = function(form, element)
@@ -24,24 +47,13 @@ add_crs_buttons = function()
         print("DEBUG: buttons: ifs_io_changeFunc")
         -- cross era opts
         if (element.tag == "cross_team1") then
-            if(element.selValue == 1) then
-                this.HeroPrefs.iHeroPlayer = 1
-            elseif(element.selValue == 2) then
-                this.HeroPrefs.iHeroPlayer = 4
-            else
-                this.HeroPrefs.iHeroPlayer = 7
-            end
+            gCrossEra.attackerName = gCrossEra.factions[element.selValue].name
         elseif (element.tag == "cross_team2") then
-            if(element.selValue == 1) then
-                this.HeroPrefs.iHeroPlayer = 1
-            elseif(element.selValue == 2) then
-                this.HeroPrefs.iHeroPlayer = 4
-            else
-                this.HeroPrefs.iHeroPlayer = 7
-            end
+            gCrossEra.defenderName = gCrossEra.factions[element.selValue].name
         end
     end
 
+    --choose which selection to display based on what was set
     --called in Enter fn for ifs_instant_options
     local original_ifs_io_GetRealValueFor = ifs_io_GetRealValueFor
     ifs_io_GetRealValueFor = function(form, tag)
@@ -55,23 +67,25 @@ add_crs_buttons = function()
         local mySliderString = nil
 
         if( tag == "cross_team1") then
-            -- only 1, 4, 7 are valid
-            if(this.HeroPrefs.iHeroPlayer == 1) then
-                val = 1
-            elseif(this.HeroPrefs.iHeroPlayer == 4) then
-                val = 2
-            else --this.HeroPrefs.iHeroPlayer == 7
-                val = 3
+
+            val = 1
+            local numElements = table.getn(gCrossEra.factions)
+            for i = 1, numElements do
+                if gCrossEra.factions[i].name == gCrossEra.attackerName then
+                    val = i
+                end
             end
+
         elseif ( tag == "cross_team2") then
-            -- only 1, 4, 7 are valid
-            if(this.HeroPrefs.iHeroPlayer == 1) then
-                val = 1
-            elseif(this.HeroPrefs.iHeroPlayer == 4) then
-                val = 2
-            else --this.HeroPrefs.iHeroPlayer == 7
-                val = 3
+
+            val = 2
+            local numElements = table.getn(gCrossEra.factions)
+            for i = 1, numElements do
+                if gCrossEra.factions[i].name == gCrossEra.defenderName then
+                    val = i
+                end
             end
+
         end
 
         if (val) then
@@ -85,9 +99,6 @@ add_crs_buttons = function()
         end
     end
 
-    --TODO: have not seen the below functions called in the logs
-
-    print("DEBUG: ifs_io_GetElementLayoutFor is " .. tostring(ifs_io_GetElementLayoutFor))
     --This is called in ifs_instant_options build screen
     local original_ifs_io_GetElementLayoutFor = ifs_io_GetElementLayoutFor
     ifs_io_GetElementLayoutFor = function(tagName, screen)
@@ -105,7 +116,12 @@ add_crs_buttons = function()
         tprint(tagTable)
         local this = screen
 
-        local factions = { "Republic", "CIS", "Empire", "Rebels" }
+
+
+        local factions = { }
+        for _, entry in gCrossEra.factions do
+            table.insert(factions, entry.string)
+        end
 
         --Do stuff with new buttons
         if (tagName == "cross_team1") then
@@ -129,6 +145,7 @@ add_crs_buttons = function()
         return tagTable
     end
 
+    --shows the specified string for the button
     --called in ifs_instant_options_layout
     local original_ifs_instant_options_PopulateItem = ifs_instant_options_PopulateItem
     ifs_instant_options_PopulateItem = function(Dest, Tag, bSelected, iColorR, iColorG, iColorB, fAlpha)
@@ -142,35 +159,24 @@ add_crs_buttons = function()
         local OnStr = ScriptCB_getlocalizestr("common.on")
         local OffStr = ScriptCB_getlocalizestr("common.off")
 
-        --TODO set correct strings
         if (Tag == "cross_team1") then
-            if (this.GamePrefs.bHeroesEnabled) then
-                if (this.HeroPrefs.iHeroPlayer == 1) then
-                    ShowStr = "ifs.mp.heroopts.heroplayer1"
-                elseif (this.HeroPrefs.iHeroPlayer == 2) then
-                    ShowStr = "ifs.mp.heroopts.heroplayer2"
-                elseif (this.HeroPrefs.iHeroPlayer == 3) then
-                    ShowStr = "ifs.mp.heroopts.heroplayer3"
-                elseif (this.HeroPrefs.iHeroPlayer == 4) then
-                    ShowStr = "ifs.mp.heroopts.heroplayer4"
+
+            local numElements = table.getn(gCrossEra.factions)
+            for i = 1, numElements do
+                if gCrossEra.factions[i].name == gCrossEra.attackerName then
+                    ShowStr = gCrossEra.factions[i].string
                 end
-            else
-                ShowStr = ""
             end
+
         elseif (Tag == "cross_team2") then
-            if (this.GamePrefs.bHeroesEnabled) then
-                if (this.HeroPrefs.iHeroPlayer == 1) then
-                    ShowStr = "ifs.mp.heroopts.heroplayer1"
-                elseif (this.HeroPrefs.iHeroPlayer == 2) then
-                    ShowStr = "ifs.mp.heroopts.heroplayer2"
-                elseif (this.HeroPrefs.iHeroPlayer == 3) then
-                    ShowStr = "ifs.mp.heroopts.heroplayer3"
-                elseif (this.HeroPrefs.iHeroPlayer == 4) then
-                    ShowStr = "ifs.mp.heroopts.heroplayer4"
+
+            local numElements = table.getn(gCrossEra.factions)
+            for i = 1, numElements do
+                if gCrossEra.factions[i].name == gCrossEra.defenderName then
+                    ShowStr = gCrossEra.factions[i].string
                 end
-            else
-                ShowStr = ""
             end
+
         end
 
         if (ShowUStr) then
@@ -190,44 +196,53 @@ add_crs_buttons = function()
     end
 
     --called in input functions to ifs_instant_options
+    -- update selection based on controller input
+    -- seems like this is only useful on consoles/ controllers, does nothing with mouse
     local original_ifs_instant_options_fnAdjustItem = ifs_instant_options_fnAdjustItem
     ifs_instant_options_fnAdjustItem = function(this, Tag, iAdjust)
 
-        print("DEBUG: buttons: ifs_instant_options_fnAdjustItem")
+        print("DEBUG: buttons: ifs_instant_options_fnAdjustItem, iAdjust is " .. tostring(iAdjust))
 
-        --TODO fix pref values
+        local numElements = table.getn(gCrossEra.factions)
+
         if (Tag == "cross_team1") then
-            if (this.HeroPrefs.iHeroPlayer == 1) then
-                if (iAdjust > 0) then
-                    this.HeroPrefs.iHeroPlayer = 4
-                end
-            elseif (this.HeroPrefs.iHeroPlayer == 4) then
-                if (iAdjust > 0) then
-                    this.HeroPrefs.iHeroPlayer = 7
-                else
-                    this.HeroPrefs.iHeroPlayer = 1
-                end
-            else
-                if (iAdjust < 0) then
-                    this.HeroPrefs.iHeroPlayer = 4
+
+            local selectedIndex = 1
+            for i = 1, numElements do
+                if gCrossEra.factions[i].name == gCrossEra.attackerName then
+                    selectedIndex = i
                 end
             end
+
+            if  (selectedIndex == numElements and iAdjust > 0)
+                    or (selectedIndex + iAdjust > numElements) then
+                gCrossEra.attackerName = gCrossEra.factions[numElements].name
+            elseif (selectedIndex == 1 and iAdjust < 0)
+                    or (selectedIndex + iAdjust < 1) then
+                gCrossEra.attackerName = gCrossEra.factions[1].name
+            else
+                gCrossEra.attackerName = gCrossEra.factions[selectedIndex + iAdjust]
+            end
+
         elseif (Tag == "cross_team2") then
-            if (this.HeroPrefs.iHeroPlayer == 1) then
-                if (iAdjust > 0) then
-                    this.HeroPrefs.iHeroPlayer = 4
-                end
-            elseif (this.HeroPrefs.iHeroPlayer == 4) then
-                if (iAdjust > 0) then
-                    this.HeroPrefs.iHeroPlayer = 7
-                else
-                    this.HeroPrefs.iHeroPlayer = 1
-                end
-            else
-                if (iAdjust < 0) then
-                    this.HeroPrefs.iHeroPlayer = 4
+
+            local selectedIndex = 2
+            for i = 1, numElements do
+                if gCrossEra.factions[i].name == gCrossEra.defenderName then
+                    selectedIndex = i
                 end
             end
+
+            if  (selectedIndex == numElements and iAdjust > 0)
+                    or (selectedIndex + iAdjust > numElements) then
+                gCrossEra.defenderName = gCrossEra.factions[numElements].name
+            elseif (selectedIndex == 1 and iAdjust < 0)
+                    or (selectedIndex + iAdjust < 1) then
+                gCrossEra.defenderName = gCrossEra.factions[1].name
+            else
+                gCrossEra.defenderName = gCrossEra.factions[selectedIndex + iAdjust]
+            end
+
         end
 
         original_ifs_instant_options_fnAdjustItem(this, Tag, iAdjust)
